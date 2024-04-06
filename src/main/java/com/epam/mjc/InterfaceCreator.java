728a20f2b2d5720bde512f12bc97012fd580d9a3
Vagharshak Samsonyan <vagosamsonyan@yahoo.com>
2024-04-07 02:35:38 +0400

    14 of 15 tests passed

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,49 +1,85 @@
 package com.epam.mjc;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 public class InterfaceCreator {
 
     public Predicate<List<String>> isValuesStartWithUpperCase() {
-        return list -> list.stream().allMatch(str -> Character.isUpperCase(str.charAt(0)));
+        return sourceList -> {
+            boolean actualResult = true;
+            for (String value : sourceList) {
+                char[] charValue = value.toCharArray();
+                int valueFirstLetter = charValue[0];
+                if (!Character.isUpperCase(valueFirstLetter)) {
+                    actualResult = false;
+                    break;
+                }
+
+            }
+            return actualResult;
+        };
     }
 
+
     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-       return list -> {
-           for (int i = 0; i < list.size(); i++) {
-               if (list.get(i) % 2 == 0) {
-                   list.add(list.get(i));
-               }
-           }
-       };
+
+        return sourceList -> {
+            List<Integer> resultList = new ArrayList<>();
+            resultList.clear();
+
+            for (int value : sourceList) {
+                if (value % 2 == 0) {
+                    resultList.add(value);
+                }
+
+            }
+            sourceList.addAll(resultList);
+        };
     }
 
     public Supplier<List<String>> filterCollection(List<String> values) {
-        return () -> values.stream()
-                .filter(str -> Character.isUpperCase(str.charAt(0)))
-                .filter(str->str.endsWith("."))
-                .filter(str->str.split(" ").length>3)
-                .collect(Collectors.toList());
+        List<String> result = new ArrayList<>();
+        {
+            return () -> {
+                for (String value : values) {
+                    if (Character.isUpperCase(value.charAt(0)) && value.endsWith(".") && value.length() > 3) {
+                        result.add(value);
+                    }
+
+                }
+                return result;
+
+            };
+        }
     }
 
     public Function<List<String>, Map<String, Integer>> stringSize() {
-        return list -> list.stream()
-                .collect(Collectors.toMap(str -> str, String::length));
+
+        return sourceKeys -> {
+            Map<String, Integer> resultMap = new HashMap<>();
+            for (String key : sourceKeys) {
+                Integer value = key.length();
+                resultMap.put(key, value);
+            }
+            return resultMap;
+        };
     }
 
+
+
     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-       return (list1,list2) -> {
-           List<Integer> result = new ArrayList<>(list1);
-           result.addAll(list2);
-           return result;
-       };
+
+
+        return (sourceList1, sourceList2) -> {
+            List<Integer> resultList = new ArrayList<>();
+            resultList.addAll(sourceList1);
+            resultList.addAll(sourceList2);
+            return resultList;
+        };
     }
 }
